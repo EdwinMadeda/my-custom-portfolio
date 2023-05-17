290e5cfb9c9a6eb054bb4538e72ef4b5f6a2efb2
EdwinMadeda <edwinmadeda@outlook.com>
2023-05-17 14:12:59 +0300

    client routing issue fixed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from './layouts/Layout';
 import Landing from './pages/Landing';
 import SingleWork from './pages/SingleWork';
 import Resume from './pages/Resume';
+import Page404 from './pages/404';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/:slug" element={<SingleWork />} />
         <Route path="/resume" element={<Resume />} />
       </Route>
+      <Route path="*" element={<Page404/>} />
     </Routes>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,15 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        bannerLight: 'url(./src/assets/images/banner-3.JPG)',
-        bannerDark: 'url(./src/assets/images/banner-2.JPG)',
+        bannerLight: 'url(../src/assets/images/banner-3.JPG)',
+        bannerDark: 'url(../src/assets/images/banner-2.JPG)',
       },
       fontFamily: {
+        default: ['DM Sans', 'Manrope', 'sans-serif'],
+        manrope: ['Manrope', 'sans-serif'],
         lemonJelly: ['LEMON_JELLY', 'cursive'],
         rainbowBridge: ['RAINBOW_BRIDGE', 'cursive'],
         roadBlast: ['ROAD_BLAST', 'serif'],
-        manrope: ['Manrope', 'sans-serif'],
       },
     },
   },
